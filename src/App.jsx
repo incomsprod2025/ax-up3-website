4ed8f0484ec7a80bc22fea2a8c322cbc1ada223a
incomsprod2025 <incomsprod2025@example.com>
2026-06-26 15:32:00 +0200

    Fix React Router basename

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import './index.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <div className="App">
         <Navbar />
         <Routes>
